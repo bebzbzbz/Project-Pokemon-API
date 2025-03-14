f16ade26a1ac4a15f01c951dc1caf54d0f504a3f
YvonneJL <yvonneloder@gmx.de>
2025-03-14 12:00:34 +0100

    PokeCardItem changed padding for large

--- src/components/PokeCardItem/PokeCardItem.tsx
+++ src/components/PokeCardItem/PokeCardItem.tsx
@@ -46,7 +46,7 @@ const PokeCardItem = ({ pokemon }: PokeCardItemProps) => {
       <h2 className="mb-5">MOVEMENTS <span>MOVEMENTS</span></h2>
       <article className="grid grid-cols-2 gap-3 justify-items-center">
         {pokemon.moves.map((move) => (
-          <p className="w-35 h-8 lg:w-40 lg:h-12 card-bg text-center rounded-xl py-1 text-poke-blue" key={crypto.randomUUID()}>
+          <p className="w-35 h-8 lg:w-40 lg:h-12 card-bg text-center rounded-xl py-1 lg:py-3 text-poke-blue" key={crypto.randomUUID()}>
             {move.move.name.includes("-") ? move.move.name.replace("-", " ").toUpperCase() : move.move.name.toUpperCase()}
             </p>
         ))}
